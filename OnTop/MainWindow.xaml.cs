edea28b1f1e006a2fb4e608ebba806e6b9cfed20
Adrian Sims <adriansims62781@gmail.com>
2022-05-06 14:42:56 +0200

    first commit

---
+++ OnTop/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OnTop
+{
+    public partial class MainWindow : Window
+    {
+        public bool AlwaysOnTop { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Deactivated += (s, e) =>
+            {
+                var wnd = s as Window;
+                wnd.Topmost = AlwaysOnTop;
+            };
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var box = sender as CheckBox;
+            AlwaysOnTop = box.IsChecked ?? false;
+        }
+    }
+}
